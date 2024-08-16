4c3061234583bdaeffee4c6d80f0d2e9fedd99ca
g.gutnik <gleb.g@itechpsp.com>
2024-08-17 01:25:36 +0300

    Login complete

--- backend/src/main/kotlin/com/violet/plugins/Serialization.kt
+++ backend/src/main/kotlin/com/violet/plugins/Serialization.kt
@@ -1,18 +1,39 @@
 package com.violet.plugins
 
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.bkbn.kompendium.core.metadata.GetInfo
+import io.bkbn.kompendium.core.plugin.NotarizedRoute
+import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.application.install
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
+import io.ktor.server.routing.route
+import io.ktor.server.routing.routing
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         json()
     }
     routing {
-        get("/json/kotlinx-serialization") {
+        route("/json/kotlinx-serialization") {
+            install(NotarizedRoute()) {
+                get = GetInfo.builder {
+                    summary("Serialization test")
+                    description("Hello world from kotlinx serialization")
+                    response {
+                        description("Check success")
+                        responseCode(HttpStatusCode.OK)
+                        responseType<Map<String, String>>()
+                    }
+                }
+            }
+
+            get {
                 call.respond(mapOf("hello" to "world"))
             }
+        }
     }
 }
