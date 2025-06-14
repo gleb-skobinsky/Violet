ea425f9bcd67394b64d5599528430df70dbb9ae0
g.gutnik <gleb.g@itechpsp.com>
2025-06-14 16:23:21 +0300

    Fix ios build

--- composeApp/src/iosMain/kotlin/org/violet/violetapp/di/startIosKoin.kt
+++ composeApp/src/iosMain/kotlin/org/violet/violetapp/di/startIosKoin.kt
@@ -1,11 +1,10 @@
 package org.violet.violetapp.di
 
-import coil3.PlatformContext
 import org.koin.core.context.startKoin
 
 @Suppress("Unused") // used in swift
 fun startIosKoin() {
     startKoin {
-        configureModules(PlatformContext.INSTANCE)
+        configureModules()
     }
 }
